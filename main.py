82323b03c48e147aac0a58db862d8bc159e1a1b3
yonatankaplan <yonakapla@gmail.com>
2025-01-19 23:05:30 -0600

    check client id

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ GET_NEWS = True  # set to True to enable the bot to do online research
 METACULUS_TOKEN = os.getenv("METACULUS_TOKEN")
 # PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 ASKNEWS_CLIENT_ID = os.getenv("ASKNEWS_CLIENT_ID")
-print(ASKNEWS_CLIENT_ID)
+print(f"client_id is:{ASKNEWS_CLIENT_ID}")
 ASKNEWS_SECRET = os.getenv("ASKNEWS_SECRET")
 # EXA_API_KEY = os.getenv("EXA_API_KEY")
 OPENAI_API_KEY = os.getenv(
