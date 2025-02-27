b6536dbeb1eef8599542026f888cbfaf7eb2f33c
yonatankaplan <yonakapla@gmail.com>
2025-02-27 13:22:17 -0600

    remove multiple choice

--- tests/test_end_2_end.py
+++ tests/test_end_2_end.py
@@ -13,23 +13,23 @@ class TestEnd2End(unittest.IsolatedAsyncioTestCase):
                 open_question_id_post_id,
                 submit_prediction=True,
                 skip_previously_forecasted_questions=False,
-                cache_seed=random.randint(0, 1000)
+                cache_seed=42
             )
         print(f"time taken to run: {datetime.datetime.now() - now}")
 
 
 
-    async def test_e2e_multiple(self):
-        open_question_id_post_id = [(22427, 22427)]
-        now = datetime.datetime.now()
-        await forecast_questions(
-                open_question_id_post_id,
-                submit_prediction=True,
-                skip_previously_forecasted_questions=False,
-                cache_seed=random.randint(0, 1000)
-            )
-        print(f"time taken to run: {datetime.datetime.now() - now}")
-
+    # async def test_e2e_multiple(self):
+    #     open_question_id_post_id = [(22427, 22427)]
+    #     now = datetime.datetime.now()
+    #     await forecast_questions(
+    #             open_question_id_post_id,
+    #             submit_prediction=True,
+    #             skip_previously_forecasted_questions=False,
+    #             cache_seed=42
+    #         )
+    #     print(f"time taken to run: {datetime.datetime.now() - now}")
+    #
 
 if __name__ == "__main__":
     unittest.main()
