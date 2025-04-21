22606511e7cad6f614d6d7f02c02ba8ce0e90cef
Alex Trejo <altrome@gmail.com>
2025-04-21 22:48:09 +0200

    use gpt-4.1 model

--- main.py
+++ main.py
@@ -360,7 +360,7 @@ if __name__ == "__main__":
         skip_previously_forecasted_questions=True,
         llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
             "default": GeneralLlm(
-                model="openai/gpt-4o-mini",
+                model="openai/gpt-4.1",
                 temperature=0.3,
                 timeout=40,
                 allowed_tries=2,
