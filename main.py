ff3d2d45375ee1a087bb7203419a11fcc7514cda
D-Enns <121063264+D-Enns@users.noreply.github.com>
2025-05-10 16:48:26 -0600

    Update main.py
    Added async def _run_forecast_on_binary() function back in. Accidentally deleted it.

--- main.py
+++ main.py
@@ -133,6 +133,53 @@ class TemplateForecaster(ForecastBot):
         response = await searcher.invoke(prompt)
         return response
 
+    async def _run_forecast_on_binary(
+        self, question: BinaryQuestion, research: str
+    ) -> ReasonedPrediction[float]:
+        prompt = clean_indents(
+            f"""
+            You are a professional forecaster interviewing for a job.
+
+            Your interview question is:
+            {question.question_text}
+
+            Question background:
+            {question.background_info}
+
+
+            This question's outcome will be determined by the specific criteria below. These criteria have not yet been satisfied:
+            {question.resolution_criteria}
+
+            {question.fine_print}
+
+
+            Your research assistant says:
+            {research}
+
+            Today is {datetime.now().strftime("%Y-%m-%d")}.
+
+            Before answering you write:
+            (a) The time left until the outcome to the question is known.
+            (b) The status quo outcome if nothing changed.
+            (c) A brief description of a scenario that results in a No outcome.
+            (d) A brief description of a scenario that results in a Yes outcome.
+
+            You write your rationale remembering that good forecasters put extra weight on the status quo outcome since the world changes slowly most of the time.
+
+            The last thing you write is your final answer as: "Probability: ZZ%", 0-100
+            """
+        )
+        reasoning = await self.get_llm("default", "llm").invoke(prompt)
+        prediction: float = PredictionExtractor.extract_last_percentage_value(
+            reasoning, max_prediction=1, min_prediction=0
+        )
+        logger.info(
+            f"Forecasted URL {question.page_url} as {prediction} with reasoning:\n{reasoning}"
+        )
+        return ReasonedPrediction(
+            prediction_value=prediction, reasoning=reasoning
+        )
+
     # Update DRE 05/10/2025 prompt
     async def _run_forecast_on_multiple_choice(
         self, question: MultipleChoiceQuestion, research: str
