a386dbd54fb4008b168e1b62e332c493832db40c
Yonatan.Kaplan <yonatan.kaplan@planckresolution.com>
2025-05-17 20:13:31 -0600

    adjusting flow and final answer

--- logic/chat_group_single_question.py
+++ logic/chat_group_single_question.py
@@ -1,7 +1,4 @@
-import json
 from typing import List, Dict, Union, Tuple
-
-import numpy as np
 
 from agents.agent_creator import create_group, create_summarization_assistant
 from logic.call_asknews import run_research
@@ -9,10 +6,10 @@ from logic.chat import validate_and_parse_response
 from logic.summarization import run_summarization_phase
 from logic.utils import extract_question_details, get_all_experts, perform_forecasting_phase, \
     format_phase1_results_to_string, \
-    perform_revised_forecasting_step, strip_title_to_filename, build_and_write_json
+    perform_revised_forecasting_step, strip_title_to_filename, build_and_write_json, get_probabilities, \
+    enrich_probabilities
 from utils.PROMPTS import GROUP_INSTRUCTIONS
 from utils.config import get_gpt_config
-from utils.utils import normalize_and_average
 
 
 async def chat_group_single_question(
@@ -37,21 +34,6 @@ async def chat_group_single_question(
     # Forecasting
     results = await perform_forecasting_phase(all_experts, question_details, news=news,
                                               is_multiple_choice=is_multiple_choice, options=options)
-    phase1_results_json = json.dumps(results, indent=2)
-
-    # Extract probabilities
-    final_probability = [result['final_probability'] for result in results.values() if 'final_probability' in result]
-    initial_probability = [result['initial_probability'] for result in results.values() if
-                           'initial_probability' in result]
-
-    final_result = int(round(np.mean(final_probability))) if not is_multiple_choice else {
-        opt: val / 100.0 for opt, val in normalize_and_average(final_probability, options=options).items()
-    }
-
-    sd_initial_step = np.std(initial_probability, ddof=1)
-    sd_deliberation_step = np.std(final_probability, ddof=1)
-    mean_initial_step = np.mean(initial_probability)
-    mean_deliberation_step = np.mean(final_probability)
 
     phase1_results_as_string = format_phase1_results_to_string(results)
 
@@ -59,7 +41,8 @@ async def chat_group_single_question(
         task=GROUP_INSTRUCTIONS.format(phase1_results_json_string=phase1_results_as_string,
                                        forecasters_list=forecasters_names))
 
-    parsed_group_results = {group_single_answer.source:validate_and_parse_response(group_single_answer.content) for group_single_answer in group_results.messages[1:]}
+    parsed_group_results = {group_single_answer.source: validate_and_parse_response(group_single_answer.content) for
+                            group_single_answer in group_results.messages if group_single_answer.source != "user"}
 
     revision_results = await perform_revised_forecasting_step(all_experts, question_details, news=news,
                                                               is_multiple_choice=is_multiple_choice, options=options)
@@ -69,19 +52,14 @@ async def chat_group_single_question(
     summarization = await run_summarization_phase(results, question_details,
                                                   summarization_assistant)
 
-    # Build final JSON
-    final_json = {
-        "question_details": question_details,
-        "date": forecast_date, "news": news,
-        "results": results,
-        "group_results": parsed_group_results,
-        "revision_results": revision_results,
-        "summary": summarization,
-        "final_result": final_result
-    }
+    # Extract probabilities
+    probabilities = get_probabilities(results, revision_results, parsed_group_results, is_multiple_choice, options)
+
+    enrich_probabilities(probabilities, question_details, news, forecast_date, summarization)
+    final_answer = probabilities['revision_mean_probability']
 
     # Save JSON
     filename = strip_title_to_filename(title)
-    await build_and_write_json(filename, final_json, is_woc)
+    await build_and_write_json(filename, probabilities, is_woc)
 
-    return final_result, summarization
+    return final_answer, summarization
