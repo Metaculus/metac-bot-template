b436471ed730344a9ec4ce1cb479e85b02fba223
D-Enns <121063264+D-Enns@users.noreply.github.com>
2025-06-01 13:01:27 -0600

    Update main.py
    fix "" around binary question

--- main.py
+++ main.py
@@ -514,7 +514,7 @@ if __name__ == "__main__":
     elif run_mode == "test_questions":
         # Example questions are a good way to test the bot's performance on a single question
         EXAMPLE_QUESTIONS = [
-            "https://www.metaculus.com/questions/18677/member-country-leaves-brics-by-2035/, # binary
+            "https://www.metaculus.com/questions/18677/member-country-leaves-brics-by-2035/", # binary
             #"https://www.metaculus.com/questions/578/human-extinction-by-2100/",  # Human Extinction - Binary
             #"https://www.metaculus.com/questions/14333/age-of-oldest-human-as-of-2100/",  # Age of Oldest Human - Numeric
             #"https://www.metaculus.com/questions/22427/number-of-new-leading-ai-labs/",  # Number of New Leading AI Labs - Multiple Choice
