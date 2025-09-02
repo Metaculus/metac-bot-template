41f699cf2ec68c3ddea48ada773486990f47dec2
Alex Trejo <altrome@gmail.com>
2025-09-02 18:02:12 +0200

    revert to live mode

--- main_with_no_framework.py
+++ main_with_no_framework.py
@@ -14,9 +14,9 @@ from config import METACULUS_TOKEN, ASKNEWS_CLIENT_ID, ASKNEWS_SECRET, EXA_API_K
 ######################### CONSTANTS #########################
 # Constants
 SUBMIT_PREDICTION = True  # set to True to publish your predictions to Metaculus
-USE_EXAMPLE_QUESTIONS = True  # set to True to forecast example questions rather than the tournament questions
+USE_EXAMPLE_QUESTIONS = False  # set to True to forecast example questions rather than the tournament questions
 NUM_RUNS_PER_QUESTION = 5  # The median forecast is taken between NUM_RUNS_PER_QUESTION runs
-SKIP_PREVIOUSLY_FORECASTED_QUESTIONS = False
+SKIP_PREVIOUSLY_FORECASTED_QUESTIONS = True
 
 
 # The tournament IDs below can be used for testing your bot.
